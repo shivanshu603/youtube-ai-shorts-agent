9471420e6ce0161c3e644aa9053efe24dc09a2a6
shivanshu603 <166390746+shivanshu603@users.noreply.github.com>
2026-04-15 17:19:37 +0530

    Create STORY_PROMPT for YouTube Shorts
    Added a prompt for generating moral stories for YouTube Shorts in Hindi.

---
+++ prompts.py
@@ -0,0 +1,21 @@
+STORY_PROMPT = """
+Create a unique and engaging moral story for YouTube Shorts.
+
+Return JSON with:
+{
+  "story_title": "",
+  "scenes": [
+    {
+      "narration": "",
+      "image_prompt": ""
+    }
+  ],
+  "youtube_title": "",
+  "description": "",
+  "tags": []
+}
+
+Language: Hindi
+Number of scenes: 5
+Narration duration: 40-50 seconds
+"""
